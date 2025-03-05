2a77801b3f34395c277cc643ccc8806435e549c6
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-05 14:25:29 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def inner(*args, **kwargs) -> Any:
+        cache_key = (tuple(args), tuple(kwargs))
+
+        if cache_key in cache_data:
+            print("Getting from cache")
+
+            return cache_data[cache_key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_data[cache_key] = result
+
+        return result
+
+    return inner
